d756e4e24fa858657a848e55697632167589df82
decollozq <decollzoq@gmail.com>
2026-03-26 21:43:44 +0900

    Design: 이전 달/다음 달 이동하기 버튼 아이콘 추가

---
+++ src/Icons.js
@@ -0,0 +1,11 @@
+export const NextIcon = () =>(
+    <svg width="27" height="32" viewBox="0 0 27 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M27 15.5885L0 31.177V4.3869e-05L27 15.5885Z" fill="currentColor"/>
+    </svg>
+)
+
+export const PrevIcon = () =>(
+    <svg width="27" height="32" viewBox="0 0 27 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 15.5885L27 4.57764e-05V31.177L0 15.5885Z" fill="currentColor"/>
+    </svg>
+)
